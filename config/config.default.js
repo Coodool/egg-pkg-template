82ac77b3cee87704c5e2dfec03bf36d055a0f4eb
coodool <yearito66079@gmail.com>
2020-07-29 00:39:18 +0800

    integrate pkg to build executable program

--- config/config.default.js
+++ config/config.default.js
@@ -1,6 +1,7 @@
 /* eslint valid-jsdoc: "off" */
 
 'use strict';
+const path = require('path');
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
@@ -10,7 +11,7 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1595521052463_7230';
@@ -23,6 +24,19 @@ module.exports = appInfo => {
     // myAppName: 'egg',
   };
 
+  // 配置运行时相关文件存储路径
+  config.rundir = process.cwd() + '/run';
+
+  // 配置日志目录
+  config.logger = {
+    dir: path.join(process.cwd(), 'logs'),
+  };
+
+  // 配置静态资源路径
+  config.static = {
+    prefix: '/',
+    dir: process.cwd() + '/public',
+  };
   return {
     ...config,
     ...userConfig,
